239fec3c327351814ba4e5f547e9533e6030f73d
dd <59326234+iisdd@users.noreply.github.com>
2021-01-10 15:44:20 +0800

    Update 1143. 最长公共子序列.py

--- 动态规划/1143. 最长公共子序列.py
+++ 动态规划/1143. 最长公共子序列.py
@@ -31,27 +31,16 @@
 1 <= text2.length <= 1000
 输入的字符串只含有小写英文字符。
 '''
-# 用时87.9%
+# 用时56.3%
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n1 = len(text1)
         n2 = len(text2)
-        dp = [[0 for _ in range(n2)] for _ in range(n1)]
-        # 先搞定两条边
-        for i in range(n1):             # 第一列
-            if (i>0 and dp[i-1][0]==1) or text1[i] == text2[0]:
-                dp[i][0] = 1
-            else:
-                dp[i][0] = 0
-        for i in range(n2):             # 第一行
-            if (i>0 and dp[0][i-1]==1) or text1[0] == text2[i]:
-                dp[0][i] = 1
-            else:
-                dp[0][i] = 0
-        # 再根据转移方程推出剩下的
-        for i in range(1, n1):
-            for j in range(1, n2):
-                if text1[i] == text2[j]:
+        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        # 根据转移方程推出dp
+        for i in range(1, n1+1):                                  # i=0 or j=0时dp为0
+            for j in range(1, n2+1):
+                if text1[i-1] == text2[j-1]:                      # i,j表示text1,text2包含的长度
                     dp[i][j] = dp[i-1][j-1] + 1
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
