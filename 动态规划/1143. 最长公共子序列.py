76f6d7db907b2956c9c9057ec1684a80f6cf5104
dd <59326234+iisdd@users.noreply.github.com>
2021-01-10 15:20:37 +0800

    Add files via upload

---
+++ 动态规划/1143. 最长公共子序列.py
@@ -0,0 +1,84 @@
+'''
+给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列。
+
+一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+
+若这两个字符串没有公共子序列，则返回 0。
+
+ 
+
+示例 1:
+
+输入：text1 = "abcde", text2 = "ace" 
+输出：3  
+解释：最长公共子序列是 "ace"，它的长度为 3。
+示例 2:
+
+输入：text1 = "abc", text2 = "abc"
+输出：3
+解释：最长公共子序列是 "abc"，它的长度为 3。
+示例 3:
+
+输入：text1 = "abc", text2 = "def"
+输出：0
+解释：两个字符串没有公共子序列，返回 0。
+ 
+
+提示:
+
+1 <= text1.length <= 1000
+1 <= text2.length <= 1000
+输入的字符串只含有小写英文字符。
+'''
+# 用时87.9%
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1)
+        n2 = len(text2)
+        dp = [[0 for _ in range(n2)] for _ in range(n1)]
+        # 先搞定两条边
+        for i in range(n1):             # 第一列
+            if (i>0 and dp[i-1][0]==1) or text1[i] == text2[0]:
+                dp[i][0] = 1
+            else:
+                dp[i][0] = 0
+        for i in range(n2):             # 第一行
+            if (i>0 and dp[0][i-1]==1) or text1[0] == text2[i]:
+                dp[0][i] = 1
+            else:
+                dp[0][i] = 0
+        # 再根据转移方程推出剩下的
+        for i in range(1, n1):
+            for j in range(1, n2):
+                if text1[i] == text2[j]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[-1][-1]
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+
+
+        
