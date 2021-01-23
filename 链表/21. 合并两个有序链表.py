b7a40a782f6e07c75174122d6bd278a3dd7c5d82
dd <59326234+iisdd@users.noreply.github.com>
2021-01-23 11:29:19 +0800

    Add files via upload

---
+++ 链表/21. 合并两个有序链表.py
@@ -0,0 +1,83 @@
+'''
+将两个升序链表合并为一个新的 升序 链表并返回。
+新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+示例 1：
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+ 
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+'''
+# 用时87.5%
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # 特殊情况
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        # 链表的开头
+        res = ListNode(-1)
+        root = res
+        # 延续新链表
+        while l1 and l2:
+            if l1.val < l2.val:
+                res.next = l1
+                l1 = l1.next
+            else:
+                res.next = l2
+                l2 = l2.next
+            res = res.next
+        # 链表的结尾
+        if l1:
+            res.next = l1
+        else:
+            res.next = l2
+        return root.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
