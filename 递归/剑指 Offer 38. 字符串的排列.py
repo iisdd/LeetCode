ea01eb87c36b7eec7702957451ef81ccc04c1969
dd <59326234+iisdd@users.noreply.github.com>
2021-07-30 15:48:57 +0800

    Add files via upload

--- 递归/剑指 Offer 38. 字符串的排列.py
+++ 递归/剑指 Offer 38. 字符串的排列.py
@@ -13,31 +13,43 @@
 '''
 # 方法一,用check列表去重
 # 用时60.3%
+# class Solution:
+#     def permutation(self, s: str) -> List[str]:
+#         self.res = []
+#         list_s = list(s)
+#         list_s.sort()
+#         n = len(list_s)
+#         check = [0 for _ in range(n)]
+#         self.traceback(list_s, '', check, n)
+#         return self.res
+#
+#     def traceback(self, li, tmp, check, n):
+#         if len(tmp) == n:
+#             self.res.append(tmp)
+#         else:
+#             for i in range(n):
+#                 if check[i]:                            # 用过的元素跳过
+#                     continue
+#                 if i>0 and li[i]==li[i-1] and check[i-1]==0:
+#                     # 当前元素与上一元素相同,且上一元素没用过,跳过
+#                     continue
+#                 check[i] = 1                            # 征用该元素
+#                 self.traceback(li, tmp+li[i], check, n)
+#                 check[i] = 0                            # 释放该元素
+
+
+# 76.6%
 class Solution:
-    def permutation(self, s: str) -> List[str]:
-        self.res = []
-        list_s = list(s)
-        list_s.sort()
-        n = len(list_s)
-        check = [0 for _ in range(n)]
-        self.traceback(list_s, '', check, n)
-        return self.res
-
-    def traceback(self, li, tmp, check, n):
-        if len(tmp) == n:
-            self.res.append(tmp)
-        else:
-            for i in range(n):
-                if check[i]:                            # 用过的元素跳过
+    def permutation(self, s):
+        li_str = list(s)
+        li_str.sort()
+        res = [""]
+        def loop(tmp, li):
+            if (li==[] and tmp != res[-1]):         # 去重1
+                res.append(tmp)
+            for i in range(len(li)):
+                if i >= 1 and li[i] == li[i - 1]:   # 去重2
                     continue
-                if i>0 and li[i]==li[i-1] and check[i-1]==0:
-                    # 当前元素与上一元素相同,且上一元素没用过,跳过
-                    continue
-                check[i] = 1                            # 征用该元素
-                self.traceback(li, tmp+li[i], check, n)
-                check[i] = 0                            # 释放该元素
-
-
-
-
-
+                loop(tmp + li[i], li[:i] + li[i + 1:])
+        loop("", li_str)
+        return res[1:]
