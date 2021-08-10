ac9e02ea37b094fb3226c5142b93027dcd5b9df5
dd <59326234+iisdd@users.noreply.github.com>
2021-08-10 09:56:16 +0800

    Add files via upload

--- 递归/剑指 Offer 40. 最小的k个数.py
+++ 递归/剑指 Offer 40. 最小的k个数.py
@@ -15,7 +15,7 @@
 0 <= k <= arr.length <= 10000
 0 <= arr[i]<= 10000
 '''
-# 方法一: 最小堆(heapq),88.1%, 复杂度O(nlogk), 遍历n个元素, 每次堆排序需要logk次操作
+# 方法一: BIF最小堆(heapq),88.1%, 复杂度O(nlogk), 遍历n个元素, 每次堆排序需要logk次操作
 class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
         import heapq
@@ -26,34 +26,30 @@ class Solution:
             heapq.heappop(li)
         return [-x for x in li]
 
+
 # 方法二: 类似快排,找出第k小的idx然后把比它小的放左边,55.6%
 class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
-        # 特殊边界
+        # 类似快排,但子集不需要有序
+        # 由于递归返回标准值的左边,所以k正好等于长度要单独列出来
         if k == len(arr): return arr
-        def loop(s, e):             # s:起点, e:终点
-            # 把arr从s开始到e,以标称值为中间点切割成两半(左边值都比中间点小,右边大)
-            m = s                   # m为标称值的idx(取第一个数)
-            i, j = s, e
-            while i < j:            # 先移右边,因为m选的左边的数,如果选的右边的数就是先移左边
-                while i < j and arr[j] >= arr[m]:
-                    j -= 1
-                while i < j and arr[i] <= arr[m]:
-                    i += 1
-                if i < j:
-                    arr[i], arr[j] = arr[j], arr[i]     # 快排,左右互换保持左小右大
-            arr[m], arr[i] = arr[i], arr[m]             # 中间值归位
-            if i == k:
-                return arr[ : i]
-            elif i > k:
-                return loop(s, i-1)
-            else:
-                return loop(i+1, e)
+        def loop(l, r):             # 对区间(l, r)排序
+            mid = l                 # 中间基准值坐标
+            i, j = l, r
+            while i < j:
+                while i < j and arr[j] >= arr[mid]: j -= 1
+                while i < j and arr[i] <= arr[mid]: i += 1
+                # 现在i的位置上是一个大于基准值的数,j的位置上是一个小于基准值的数,或者i == j了,所以直接换就行了
+                arr[i], arr[j] = arr[j], arr[i]
+            # 左右搞定了,把中间的数归位
+            # 因为是先动的j再动的i,所以arr[i]肯定是 <= arr[mid]的,直接换就行
+            # 终止while循环分两种情况
+            # 1.j直接冲到i的位置,上次i停下来的位置肯定有arr[i]<arr[mid](换过的) or arr[i] == arr[mid](i没动过)
+            # 2.j停了i往右到j的位置,j停的位置肯定有arr[j]
+            arr[mid], arr[i] = arr[i], arr[mid]
+            if i > k:
+                return loop(l, i-1)
+            elif i < k:
+                return loop(i+1, r)
+            return arr[:i]
         return loop(0, len(arr)-1)
-
-
-
-
-
-
-
